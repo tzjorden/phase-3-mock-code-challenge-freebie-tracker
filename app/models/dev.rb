54bbb1a64279a3203849235895adcf966e66ea04
Tenzin Jorden <tenden96@gmail.com>
2022-03-02 21:07:07 -0800

    Done

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,24 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    #received_one?(item_name)
+    # accepts an item_name (string) and returns true if any of the freebies associated with the dev has that item_name, otherwise returns false
+
+    def received_one?(item_name)
+        self.freebies.any? do |freebie|
+            freebie.item_name == item_name
+         
+            # !!self.freebies.find_by(item_name: item_name) or this works too instead of the two lines above
+        end
+    end
+
+
+    
+    def give_away(freebie,dev)
+        freebie.update(dev: dev) unless freebie.dev != self
+
+        # if freebie.dev_id == self.id         another way of doing the code above
+        #     freebie.update(dev_id : dev.id) 
+    end
 end
